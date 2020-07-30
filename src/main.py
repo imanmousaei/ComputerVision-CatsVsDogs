ece6b12db9e4e7545bddddd4eb560db84234c2ee
Iman Mousaei <imanmousaei1379@gmail.com>
2020-07-30 11:53:44 +0430

    organize data sets

--- src/main.py
+++ src/main.py
@@ -1,4 +1,29 @@
 import os
+import random
+import glob
+import shutil
 
-os.chdir('../res/dogs_vs_cats')  # changes directory
+os.chdir('../res/dogs-vs-cats/train')  # changes directory
+
+
+os.makedirs('train/cats')
+os.makedirs('train/dogs')
+os.makedirs('validation/cats')
+os.makedirs('validation/dogs')
+os.makedirs('test/cats')
+os.makedirs('test/dogs')
+
+
+for image in random.sample(glob.glob('cat*'), 500):  # moves 500 random cat images to train/cats
+    shutil.move(image, '../train/cats')
+for image in random.sample(glob.glob('dog*'), 500):
+    shutil.move(image, '../train/dogs')
+for image in random.sample(glob.glob('cat*'), 100):
+    shutil.move(image, '../validation/cats')
+for image in random.sample(glob.glob('dog*'), 100):
+    shutil.move(image, '../validation/dogs')
+for image in random.sample(glob.glob('cat*'), 50):
+    shutil.move(image, '../test/cats')
+for image in random.sample(glob.glob('dog*'), 50):
+    shutil.move(image, '../test/dogs')
 
